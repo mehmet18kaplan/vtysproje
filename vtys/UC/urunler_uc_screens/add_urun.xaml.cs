2d41b9cca5d75c1541411a5f41b220586da85979
emre-usta <emre.usta@bil.omu.edu.tr>
2023-01-18 23:15:41 +0300

    satis login page revise

---
+++ vtys/UC/urunler_uc_screens/add_urun.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vtys.UC.satis_uc_screens
+{
+    /// <summary>
+    /// add_urun.xaml etkileşim mantığı
+    /// </summary>
+    public partial class add_urun : Window
+    {
+        public add_urun()
+        {
+            InitializeComponent();
+        }
+    }
+}
